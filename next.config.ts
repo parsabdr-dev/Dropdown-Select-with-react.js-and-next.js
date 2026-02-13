798ac37cf2cca9543ec09c125023da7b1aeae29c
parsabdr-dev <parsabadri82@gmail.com>
2026-02-14 00:13:17 +0330

    making it work

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/test-select',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
